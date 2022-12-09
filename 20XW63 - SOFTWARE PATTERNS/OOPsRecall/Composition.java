ab2acbf0462b8aaca07158bb0cb9835219dfbdba
Karthickpranav S N <76254926+jassuwu@users.noreply.github.com>
2022-12-09 15:04:58 +0530

    20XW63: fix composition implementation in OOPs recall

--- 20XW63 - SOFTWARE PATTERNS/OOPsRecall/Composition.java
+++ 20XW63 - SOFTWARE PATTERNS/OOPsRecall/Composition.java
@@ -4,34 +4,34 @@ public class Composition {
     }
 }
 
-class Page {
-    private int number;
-    private String material;
-
-    public Page(int number, String material) {
-        this.number = number;
-        this.material = material;
-    }
-
-    public int getNumber() {
-        return this.number;
-    }
-
-    public void setNumber(int number) {
-        this.number = number;
-    }
-
-    public String getMaterial() {
-        return this.getMaterial;
-    }
-
-    public void setMaterial(String material) {
-        this.material = material;
-    }
-}
-
-
 class Book {
+    private class Page {
+        private int number;
+        private String material;
+
+        public Page(int number, String material) {
+            this.number = number;
+            this.material = material;
+        }
+
+        public int getNumber() {
+            return this.number;
+        }
+
+        public void setNumber(int number) {
+            this.number = number;
+        }
+
+        public String getMaterial() {
+            return this.getMaterial;
+        }
+
+        public void setMaterial(String material) {
+            this.material = material;
+        }
+    }
+
+
     private String title;
     private Page[] pages;
 
