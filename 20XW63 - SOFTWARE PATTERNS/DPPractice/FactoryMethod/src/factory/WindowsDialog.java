d1dc835bd0273a3ac5236f930db86773cdaa4a8f
jassuwu <20pw14@psgtech.ac.in>
2022-12-16 06:53:16 +0530

    20XW63: practice FactoryMethod DP

---
+++ 20XW63 - SOFTWARE PATTERNS/DPPractice/FactoryMethod/src/factory/WindowsDialog.java
@@ -0,0 +1,13 @@
+package factory;
+
+import buttons.Button;
+import buttons.WindowsButton;
+
+public class WindowsDialog extends Dialog {
+
+    @Override
+    public Button createButton() {
+        return new WindowsButton();
+    }
+
+}
