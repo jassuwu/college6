299d891f117b445dcd429ec4c6cde8232c213750
jassuwu <20pw14@psgtech.ac.in>
2022-12-20 10:11:45 +0530

    20XW67: start UCS implementation

--- 20XW67 - ARTIFICIAL INTELLIGENCE LAB/UCS/WeightedDigraph.py
+++ 20XW67 - ARTIFICIAL INTELLIGENCE LAB/UCS/WeightedDigraph.py
@@ -10,11 +10,19 @@ class WeightedDigraph:
     def E(self):
         return self.edges
 
+    def adj(self, v):
+        return self.adjList[v]
+
     def addEdge(self, fromNode, toNode, weight):
-        self.adjList[fromNode].append((toNode, weight))
+        self.adjList[fromNode].append({toNode: weight})
         self.edges+=1
 
-    def UCS(self, v):
-        pass
-
-    
+    def UCS(self, v, goal):
+        marked = [False for i in range(self.V())]
+        edgeTo = [None for i in range(self.V())]
+        q = []
+        q.append(v)
+        marked[v] = True
+        while v != goal:
+            pass
+            
