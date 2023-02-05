1bc124c0ad007ab1212f88a0b1ba14aaee7669b8
20PW14 <karthickprnv@gmail.com>
2023-02-05 13:12:58 +0530

    20XW68: rmiPractice review

--- 20XW68 - DISTRIBUTED ENTERPRISE COMPUTING LAB/Practice/rmiPractice/src/RMIClient.java
+++ 20XW68 - DISTRIBUTED ENTERPRISE COMPUTING LAB/Practice/rmiPractice/src/RMIClient.java
@@ -1,19 +1,14 @@
 import java.rmi.*;
-import java.rmi.registry.*;
-public class RMIClient
-{
-    public static void main(String[] arguments)
-    {
-        try
-        {
-            System.setProperty("java.rmi.server.hostname","127.0.0.1");
-            MethodImpl methodImpl=(MethodImpl)Naming.lookup("rmi://localhost:7775/sqrt");
-            double dbl=methodImpl.getSQRT(100);
-            System.out.println("SQRT:"+dbl);
+
+public class RMIClient {
+    public static void main(String[] arguments) {
+        try {
+            System.setProperty("java.rmi.server.hostname", "127.0.0.1");
+            MethodImpl methodImpl = (MethodImpl) Naming.lookup("rmi://localhost:7775/sqrt");
+            double dbl = methodImpl.getSQRT(100);
+            System.out.println("SQRT:" + dbl);
+        } catch (Exception exec) {
+            System.out.println("Error--" + exec.toString());
         }
-        catch(Exception exec)
-        {
-            System.out.println("Error--" +exec.toString());
-        }
-    }//End of main() method
-}//End of RMIClient class
+    }// End of main() method
+}// End of RMIClient class
--- 20XW68 - DISTRIBUTED ENTERPRISE COMPUTING LAB/Practice/rmiPractice/src/RMIServer.java
+++ 20XW68 - DISTRIBUTED ENTERPRISE COMPUTING LAB/Practice/rmiPractice/src/RMIServer.java
@@ -6,22 +6,18 @@ public class RMIServer extends UnicastRemoteObject implements MethodImpl {
     public RMIServer() throws RemoteException {
         System.out.println("The server is instantiated.");
     }
-    public double getSQRT(double dbl)
-    {
+
+    public double getSQRT(double dbl) {
         return Math.sqrt(dbl);
     }
-    public static void main(String[] arguments)
-    {
-        try
-        {
-            RMIServer server=new RMIServer();
-            Registry registry = LocateRegistry.createRegistry(7775);
-            registry.bind("sqrt",new RMIServer());
-        }
-        catch(Exception e)
-        {
-            System.out.println("Error -- " +e.toString());
-        }
-    }//End of main() method
-}//End of RMIServer class
 
+    public static void main(String[] arguments) {
+        try {
+            RMIServer server = new RMIServer();
+            Registry registry = LocateRegistry.createRegistry(7775);
+            registry.bind("sqrt", server);
+        } catch (Exception e) {
+            System.out.println("Error -- " + e.toString());
+        }
+    }// End of main() method
+}// End of RMIServer class
